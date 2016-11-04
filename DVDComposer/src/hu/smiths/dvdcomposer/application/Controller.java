5a764318dc0922cc96d253e1f63fda8a189e3a81
Kovács Zsolt <marament@gmail.com>
2016-11-04 19:53:22 +0100

    JavaFX

---
+++ DVDComposer/src/hu/smiths/dvdcomposer/application/Controller.java
@@ -0,0 +1,29 @@
+package hu.smiths.dvdcomposer.application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+	@FXML
+	TextField wasdField;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void btnSubmit(ActionEvent actionEvent) {
+    }
+
+    public void setVotingEnabledState(boolean enabled) {
+    }
+
+
+}
--- DVDComposer/src/hu/smiths/dvdcomposer/application/MainApplication.java
+++ DVDComposer/src/hu/smiths/dvdcomposer/application/MainApplication.java
@@ -1,14 +1,28 @@
 package hu.smiths.dvdcomposer.application;
 
 import hu.smiths.dvdcomposer.utils.JarLoader;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class MainApplication {
+public class MainApplication extends Application {
 
+	@Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("layout_main.fxml"));
+        primaryStage.setTitle("RedisVoteFX");
+        primaryStage.setScene(new Scene(root, 640, 480));
+        primaryStage.show();
+    }
+	
 	public static void main(String[] args) {
 		JarLoader loader = new JarLoader();
-		loader.load();		
+//		loader.load();		
 
 		System.out.println("Succes!");
+		launch(args);
 	}
 
 }
