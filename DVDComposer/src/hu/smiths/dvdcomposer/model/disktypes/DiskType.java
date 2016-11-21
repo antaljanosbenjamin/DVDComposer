8f2f6ea3dd2c2030f21d741aec6251c581a5add6
Benjamin János Antal <jank12ultra@gmail.com>
2016-11-22 00:21:44 +0100

    Remove unnecesearry disk type classes, add create method to DiskType

--- DVDComposer/src/hu/smiths/dvdcomposer/model/disktypes/DiskType.java
+++ DVDComposer/src/hu/smiths/dvdcomposer/model/disktypes/DiskType.java
@@ -1,70 +1,56 @@
 package hu.smiths.dvdcomposer.model.disktypes;
 
 import java.io.Serializable;
+import java.util.TreeSet;
 
 public class DiskType implements Serializable {
 
 	private static final long serialVersionUID = 6525241729717592702L;
-	
-	private static Integer globalId = 0;
 
 	private String name;
-	
+
 	private Integer size;
-	
+
 	private Integer count;
-	private final Integer id;
-	
+
 	private Boolean infinity;
-	
-	DiskType(String name, Integer size, Integer count, Boolean infinity){
+
+	public static DiskType createFinite(String name, Integer size, Integer count) {
+		return new DiskType(name, size, count, Boolean.FALSE);
+	}
+
+	public static DiskType createInfinite(String name, Integer size) {
+		return new DiskType(name, size, Integer.valueOf(0), Boolean.TRUE);
+	}
+
+	protected DiskType(String name, Integer size, Integer count, Boolean infinity) {
 		this.name = name;
 		this.size = size;
 		this.count = count;
 		this.infinity = infinity;
-		synchronized (globalId) {
-			id = globalId;
-			globalId++;
-		}
-	}
-	
-	public String getName() {
-		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getName() {
+		return name;
 	}
 
 	public Integer getSize() {
 		return size;
 	}
 
-	public void setSize(Integer size) {
-		this.size = size;
+	public Boolean countIsAtLeast(Integer guessedCount) {
+		return infinity || count >= guessedCount;
 	}
 
-	public Integer getCount() {
-		return count;
+	public Boolean countIsGreater(Integer guessedCount) {
+		return infinity || count > guessedCount;
 	}
 
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-
-	public Boolean getInfinity() {
-		return infinity;
-	}
-
-	public void setInfinity(Boolean infinity) {
-		this.infinity = infinity;
-	}
-	
 	@Override
-	public boolean equals(Object otherObject){
-		if (otherObject.getClass().equals(getClass())){
+	public boolean equals(Object otherObject) {
+		if (otherObject.getClass().equals(getClass())) {
 			DiskType otherType = (DiskType) otherObject;
-			return otherType.id.equals(this.id);
+			return otherType.name.equals(this.name) && otherType.size.equals(this.size);
 		} else
 			return false;
 	}
