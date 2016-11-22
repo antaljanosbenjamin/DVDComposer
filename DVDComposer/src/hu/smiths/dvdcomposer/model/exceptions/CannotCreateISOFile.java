69baa6f09b488d14f964af6d6b2545a2b5e4418f
Benjamin János Antal <jank12ultra@gmail.com>
2016-11-22 23:29:34 +0100

    Add working ISOWriter

---
+++ DVDComposer/src/hu/smiths/dvdcomposer/model/exceptions/CannotCreateISOFile.java
@@ -0,0 +1,11 @@
+package hu.smiths.dvdcomposer.model.exceptions;
+
+public class CannotCreateISOFile extends Exception {
+
+	private static final long serialVersionUID = 7742279015938528249L;
+
+	public CannotCreateISOFile(Throwable cause) {
+		super(cause);
+	}
+
+}
