e25894aabc623189fbc476c92e61a4c807d62be1
Benjamin János Antal <jank12ultra@gmail.com>
2016-11-22 03:22:03 +0100

    Add exception classes

---
+++ DVDComposer/src/hu/smiths/dvdcomposer/model/exceptions/TooLargeFolderException.java
@@ -0,0 +1,10 @@
+package hu.smiths.dvdcomposer.model.exceptions;
+
+public class TooLargeFolderException extends RuntimeException {
+
+	private static final long serialVersionUID = 7702656815091944811L;
+
+	public TooLargeFolderException(String message){
+		super(message);
+	}
+}
