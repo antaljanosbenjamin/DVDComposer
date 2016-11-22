c2aa0a6ccf39ed6ff66a15e02ae9f4c788f96c36
Benjamin János Antal <jank12ultra@gmail.com>
2016-11-22 21:54:58 +0100

    Add Input, OuterAlgorithm and GreedyAlgorithm and change Algorithm

--- DVDComposer/src/hu/smiths/dvdcomposer/model/algorithm/Algorithm.java
+++ DVDComposer/src/hu/smiths/dvdcomposer/model/algorithm/Algorithm.java
@@ -1,5 +1,11 @@
 package hu.smiths.dvdcomposer.model.algorithm;
 
-public interface Algorithm {
+import java.util.Set;
 
+import hu.smiths.dvdcomposer.model.Disc;
+import hu.smiths.dvdcomposer.model.exceptions.CannotFindValidAssignmentException;
+
+public interface Algorithm {
+	
+	public Set<Disc> generate(Input input) throws CannotFindValidAssignmentException;
 }
---
+++ DVDComposer/src/hu/smiths/dvdcomposer/model/algorithm/GreedyAlgorithm.java
@@ -0,0 +1,15 @@
+package hu.smiths.dvdcomposer.model.algorithm;
+
+import java.util.Set;
+
+import hu.smiths.dvdcomposer.model.Disc;
+
+public class GreedyAlgorithm implements Algorithm {
+
+	@Override
+	public Set<Disc> generate(Input input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
