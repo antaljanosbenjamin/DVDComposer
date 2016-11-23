e3dd87405a8dcf0d945b93603662d0884977a60e
Kovács Zsolt <marament@gmail.com>
2016-11-23 23:42:10 +0100

    COmmit

--- DVDComposer/src/hu/smiths/dvdcomposer/view/MainViewController.java
+++ DVDComposer/src/hu/smiths/dvdcomposer/view/MainViewController.java
@@ -1,23 +1,18 @@
 package hu.smiths.dvdcomposer.view;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import hu.smiths.dvdcomposer.view.extensions.NumberTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 public class MainViewController implements Initializable{
 
