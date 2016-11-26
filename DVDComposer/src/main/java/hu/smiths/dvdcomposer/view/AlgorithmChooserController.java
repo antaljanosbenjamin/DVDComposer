467f17bc6fb442c3a08614c4ce31b9cc046da670
Kovács Zsolt <marament@gmail.com>
2016-11-26 22:39:46 +0100

    Add View fixes

--- DVDComposer/src/main/java/hu/smiths/dvdcomposer/view/AlgorithmChooserController.java
+++ DVDComposer/src/main/java/hu/smiths/dvdcomposer/view/AlgorithmChooserController.java
@@ -1,8 +1,6 @@
 package hu.smiths.dvdcomposer.view;
 
 import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 import hu.smiths.dvdcomposer.model.ModelManager;
 import hu.smiths.dvdcomposer.model.algorithm.OuterAlgorithm;
@@ -24,16 +22,7 @@ public class AlgorithmChooserController extends ModelController {
 	@FXML
 	TextField classNameField;
 
-	OuterAlgorithm algorithm;
-
 	File selectedJar;
-
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		if (algorithm != null) {
-			algorithm = (OuterAlgorithm) ModelManager.getModel().getAlgorithm();
-		}
-	}
 
 	public void chooseAlgorithm(ActionEvent event) {
 		FileChooser chooser = new FileChooser();
@@ -44,13 +33,19 @@ public class AlgorithmChooserController extends ModelController {
 		}
 	}
 
-	public void generateAlgorithm(ActionEvent event) {
+	private void generateAlgorithm() {
+		try {
+			ModelManager.getModel()
+					.setAlgorithm(OuterAlgorithm.createFromJarAndClassFQN(selectedJar, classNameField.getText()));
+		} catch (CannotLoadAlgorithmClass e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void next(ActionEvent event) {
 		if (classNameField != null && selectedJar != null) {
-			try {
-				algorithm = OuterAlgorithm.createFromJarAndClassFQN(selectedJar, classNameField.getText());
-			} catch (CannotLoadAlgorithmClass e) {
-				e.printStackTrace();
-			}
+			generateAlgorithm();
+			SceneManager.getInstance().changeScene("/fxml/resultView.fxml");
 		} else {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("Alert!");
@@ -60,22 +55,11 @@ public class AlgorithmChooserController extends ModelController {
 		}
 	}
 
-	public void next(ActionEvent event) {
-		refreshModel();
-		SceneManager.getInstance().changeScene("/fxml/algorithChooserView.fxml");
-	}
-
 	public void prev(ActionEvent event) {
-		refreshModel();
 		SceneManager.getInstance().changeScene("/fxml/mainView.fxml");
 	}
 
-	private void refreshModel() {
-		ModelManager.getModel().setAlgorithm(algorithm);
-	}
-
 	public void saveModelAction(ActionEvent event) {
-		refreshModel();
 		saveModel(event);
 	}
 
